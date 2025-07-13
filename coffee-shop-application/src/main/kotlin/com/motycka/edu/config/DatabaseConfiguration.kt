6f501c5f18f93f2cc37679c811e2c6e505fa4cbf
mira-996 <83557906+mira-996@users.noreply.github.com>
2025-07-13 20:11:38 +0400

    Update DatabaseConfiguration.kt

--- coffee-shop-application/src/main/kotlin/com/motycka/edu/config/DatabaseConfiguration.kt
+++ coffee-shop-application/src/main/kotlin/com/motycka/edu/config/DatabaseConfiguration.kt
@@ -100,3 +100,5 @@ fun Application.configureDatabases() {
 
 suspend fun <T> suspendTransaction(block: Transaction.() -> T): T =
     newSuspendedTransaction(Dispatchers.IO, statement = block)
+suspend fun <T> dbQuery(block: suspend () -> T): T =
+    newSuspendedTransaction(Dispatchers.IO) { block() }
